9fdd9314b2d0edd97ab455b76f915dcef6501eba
SpaaJ <japsouli@hotmail.fr>
2025-12-24 14:54:08 +0100

    movies springboot creation

---
+++ movies/springboot/src/main/java/com/movies/springboot/model/Product.java
@@ -0,0 +1,16 @@
+package com.movies.springboot.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+    private String description;
+}
---
+++ movies/springboot/src/main/java/com/movies/springboot/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.movies.springboot.service;
+
+import com.movies.springboot.model.Product;
+import com.movies.springboot.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product productDetails) {
+        Product product = getProductById(id);
+        product.setName(productDetails.getName());
+        product.setPrice(productDetails.getPrice());
+        product.setDescription(productDetails.getDescription());
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
