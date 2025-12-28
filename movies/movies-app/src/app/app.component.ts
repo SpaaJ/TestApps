9e1519d954ec2822de787f5023a37ba578d2763c
SpaaJ <3485946+SpaaJ@users.noreply.github.com>
2025-12-28 13:05:43 +0100

    Updates names

--- movies/movies-app/src/app/app.component.ts
+++ movies/movies-app/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  protected readonly title = signal('angular');
+  protected readonly title = signal('movies-app');
 }
